d37950c944f587622e2deb067318ce7ae716f3ab
Leticia Gaspar <leticia.gaspar@latitudde.com>
2025-02-16 19:55:14 +0000

    routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,17 @@ import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
-    { path: 'home', component: HomeComponent },
-    { path: 'login', component: LoginComponent },
+    { path: 'home', component: HomeComponent, data: { title: 'Home' } },
+    { path: 'login', component: LoginComponent, data: { title: 'Login' } },
     { path: '', redirectTo: 'login', pathMatch: 'full' },
-    { path: 'job-performance', component: JobPerformanceComponent },
-    { path: 'calls', component: CallsComponent },
-    { path: 'details/:id', component: CallComponent },
-];
+    { path: 'job-performance', component: JobPerformanceComponent, data: { title: 'Job Performance' } },
+    {
+      path: 'calls',
+      component: CallsComponent,
+      data: { title: 'Calls' },
+      children: [
+        { path: 'details/:id', component: CallDetailsComponent, data: { title: 'Call Details' } }
+      ]
+    }
+  ];
+  
